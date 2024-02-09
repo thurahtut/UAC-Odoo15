361a58d4df5a166d35ee3ec032ef615d8862b13d
thurahtut <thurahtut@sssretail.com>
2024-02-09 11:54:21 +0630

    develope Update: vendor_extension module

--- vendor_extension/models/res_partner.py
+++ vendor_extension/models/res_partner.py
@@ -4,4 +4,10 @@ from odoo import _, api, fields, models, tools
 class ResPartnerVendorInherit(models.Model):
     _inherit = 'res.partner'
 
-    supplier_type = fields.Selection([('trade_supplier', 'Trade Supplier'),('non_trade_supplier', 'Non Trade Supplier')])
+    supplier_type = fields.Selection([('trade_supplier', 'Trade Supplier'),('non_trade_supplier', 'Non Trade Supplier')], string='Supplier Type')
+    
+    # @api.model
+    # def create(self, values):
+    #     result = super().create(values)
+        
+    #     return result
