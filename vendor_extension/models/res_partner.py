d81c4723d2d98f93afd3d95629a6724e96e02269
thurahtut <thurahtut@sssretail.com>
2024-03-26 08:18:40 +0630

    add customer_type into res_partner

--- vendor_extension/models/res_partner.py
+++ vendor_extension/models/res_partner.py
@@ -5,6 +5,7 @@ class ResPartnerVendorInherit(models.Model):
     _inherit = 'res.partner'
 
     supplier_type = fields.Selection([('trade_supplier', 'Trade Supplier'),('non_trade_supplier', 'Non Trade Supplier')], string='Supplier Type')
+    customer_type = fields.Selection([('trade_customer', 'Trade Customer'),('non_trade_customer', 'Non Trade Customer')], string='Customer Type')
     
     # @api.model
     # def create(self, values):
